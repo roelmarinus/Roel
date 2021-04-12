35c274b2172c6a39dfa27d3c53dfb8d7ff5cf58f
roelmarinus <81803283+roelmarinus@users.noreply.github.com>
2021-04-12 19:22:35 +0200

    Submit assignment Strings
    Submit assignment Strings

---
+++ main.py
@@ -0,0 +1,33 @@
+# Do not modify these lines
+__winc_id__ = '71dd124b4a6e4d268f5973db521394ee'
+__human_name__ = 'strings'
+
+# Add your code after this line
+scoorder_1= 'Ruud Gullit'
+scoorder_2= 'Marco van Basten'
+goal_1= 32
+goal_2= 54
+print('Ruud Gullit 32'+" Marco van Basten 54")
+print(f'{scoorder_1} scored in the {goal_1}nd minute \n{scoorder_2} scored in the {goal_2}th minute')
+player = "hans van breukelen"
+print(player [:4])
+print(player [5:18])
+print(f'{player[:1]}. {player [5:18]}')
+print(f'{player[:4]}! {player[:4]}! {player[:4]}!')
+print ('hans! hans! hans!'!= 'hans! hans! hans!') 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
