992bf40330ec91f293eeff0ae25ca7b11f6ad13f
roelmarinus <81803283+roelmarinus@users.noreply.github.com>
2021-04-14 09:35:48 +0200

    Submit assignment Strings
    Submit assignment Strings

---
+++ main.py
@@ -0,0 +1,45 @@
+# Do not modify these lines
+__winc_id__ = '71dd124b4a6e4d268f5973db521394ee'
+__human_name__ = 'strings'
+
+# Add your code after this line
+scoorder_1 = 'Ruud Gullit'
+scoorder_2 = 'Marco van Basten'
+goal_0 = 32
+goal_1 = 54
+scorers = 'Ruud Gullit 32'','" Marco van Basten 54"
+report = f'{scoorder_1} scored in the {goal_0}nd minute\n{scoorder_2} scored in the {goal_1}th minute'
+player = "hans van breukelen"
+first_name = player [:4]
+numbers = player.find("van breukelen")
+lastname = player [5:18]
+last_name_len = len(lastname) 
+name_short = f'{player[:1]}. {player [5:18]}'
+chant = f'{player[:4]}! {player[:4]}! {player[:4]}! {player[:4]}!'
+spatie = chant[-1]
+good_chant = chant[-1]
+good_chant = ('good_chant' != ' ')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
